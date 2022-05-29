5a5428ec570032d4150aa754f0a7dccab474da42
sirojiddin0904 <sirojiddin0904@gmail.com>
2022-05-29 11:50:28 +0500

    hikari

--- src/main/kotlin/dev/seriy/blogv2/Application.kt
+++ src/main/kotlin/dev/seriy/blogv2/Application.kt
@@ -1,17 +1,27 @@
 package dev.seriy.blogv2
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import dev.seriy.blogv2.features.blogs.configureBlogRouting
 import dev.seriy.blogv2.plugins.configureRouting
 import dev.seriy.blogv2.plugins.configureSerialization
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
+import java.io.PrintWriter
+import java.util.*
+
 
 fun main() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5434/blogv2", driver = "org.postgresql.Driver",
-        password = "ghjuhfvvf", user = "postgres"
-    )
+    val props = Properties()
+    props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource")
+    props.setProperty("dataSource.user", "qxliufstebbvlw")
+    props.setProperty("dataSource.password", "fec68196fb1633b0a475b9f097e3cf19da8e9e2340f685c7e9ddcd13167d50ab")
+    props.setProperty("dataSource.databaseName", "d8tvpluk9c1mu3")
+    props["dataSource.logWriter"] = PrintWriter(System.out)
+    val config = HikariConfig(props)
+    val ds = HikariDataSource(config)
+    Database.connect(ds)
 
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureBlogRouting()
